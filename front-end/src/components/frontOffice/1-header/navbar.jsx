11b9b145398c3e570994fa2a98cffb93950834a2
amina134 <aminakouni14@gmail.com>
2025-08-21 00:19:33 +0100

    designed the navbar

--- front-end/src/components/frontOffice/1-header/navbar.jsx
+++ front-end/src/components/frontOffice/1-header/navbar.jsx
@@ -1,40 +1,70 @@
-import './navbar.css'
+import React, { useState } from 'react';
 import { BsSearch } from "react-icons/bs";
-import { RiPokerHeartsLine } from "react-icons/ri";
 import { BsCart3 } from "react-icons/bs";
+import { HiOutlineHeart } from "react-icons/hi2";
 import { FiUser } from "react-icons/fi";
+import './navbar.css';
 
-const Navbar=()=>{
-    return(
-        <div>
-            <div>
-               <img src="/logo.png" alt="Caravan"/>
-               
+const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isSearchVisible, setIsSearchVisible] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+        // Close search when opening menu
+        if (!isMenuOpen && isSearchVisible) {
+            setIsSearchVisible(false);
+        }
+    };
+
+    const toggleSearch = () => {
+        setIsSearchVisible(!isSearchVisible);
+        // Close menu when opening search
+        if (!isSearchVisible && isMenuOpen) {
+            setIsMenuOpen(false);
+        }
+    };
+
+    return (
+        <div className='navbar'>
+            <div className='logo'>
+                <img src="/logo.png" alt="Caravan" />
             </div>
-            <div>
-               <ul>
-                <li>Home</li>
-                <li>Menu</li>
-                <li>Contact</li>
-                <li>About</li>
-               </ul>
+            
+            <div className={`nav ${isMenuOpen ? 'active' : ''}`}>
+                <ul>
+                    <li>Home</li>
+                    <li>Menu</li>
+                    <li>Contact</li>
+                    <li>About</li>
+                </ul>
             </div>
-            <div className="search-container">
-                
+            
+            <div className={`search-container ${isSearchVisible ? 'active' : ''}`}>
                 <input
                     type="text"
                     className="search-input"
-                    placeholder="What are you looking for ?"
+                    placeholder="What are you looking for?"
                 />
                 <span className="search-icon"><BsSearch /></span>
             </div>
-            <div>
-               <RiPokerHeartsLine/>
-               <BsCart3/>
-               <FiUser/>
-
+            
+            <div className='icons-nav'>
+                <div className="icon-button" onClick={toggleSearch}>
+                    <BsSearch className="search-toggle-icon" />
+                </div>
+                <HiOutlineHeart />
+                <BsCart3 />
+                <FiUser className='user-icon' />
+            </div>
+            
+            <div className={`hamburger ${isMenuOpen ? 'active' : ''}`} onClick={toggleMenu}>
+                <span className='span-ham'></span>
+                <span className='span-ham'></span>
+                <span className='span-ham'></span>
             </div>
         </div>
-    )
-}
-export default Navbar
+    );
+};
+
+export default Navbar;
